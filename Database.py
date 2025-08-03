2ef72da7855becff23c3c0592573a944b2a27949
Ahnd6474 <ahnd6474@gmail.com>
2025-08-03 22:28:26 +0900

    update

--- Database.py
+++ Database.py
@@ -7,6 +7,30 @@ def read_fasta(filename):
     for record in SeqIO.parse(filename, 'fasta'):
         data.append(Protein(record.id, record.description, record.locus, record.organism, record.seq))
     return data
+
 class ProteinDB:
-    def __init__(self):
+    def __init__(self,fasta=None,protein=None):
+        if protein is None:
+            protein=[]
+        if fasta is None:
+            fasta=[]
+        else:
+            read_fasta(fasta)
+        self.proteins=fasta+protein
+        self.proteins.sort()
+
+    def __len__(self):
+        return len(self.proteins)
+    def __getitem__(self, index):
+        return self.proteins[index]
+    def __iter__(self):
+        return iter(self.proteins)
+    def __contains__(self, item):
+        return item in self.proteins
+    def __add__(self, other):
+        self.proteins.extend(other.proteins)
+        self.proteins=list(set(self.proteins))
+        self.proteins.sort()
+    def query(self,query):
+        
 
--- Protein.py
+++ Protein.py
@@ -44,28 +44,3 @@ def describe(description):
 
     return accession,description,locus,organism
 
-
-def comb_sort(l, s):
-    n = len(l)
-    gap = n
-    while True:
-        gap = int(gap // s) if gap // s > 1 else 1
-        c = 0 if gap == 1 else 1
-        for i in range(n - gap):
-            if l[i] > l[i + gap]:
-                l[i], l[i + gap] = l[i + gap], l[i]
-                c += 1
-        if c == 0:
-            break
-    return l
-
-def insertion_sort(l):
-    n=len(l)
-    for i in range(n):
-        t=l[i]
-        c=i
-        while c>0 and l[c-1]>=t:
-            l[c]=l[c-1]
-            c-=1
-        l[c]=t
-    return l
