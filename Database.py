43b7719c828577e351b29a582bf07a23c21c42d2
Danny <ahnd6474@gmail.com>
2025-08-04 14:48:56 +0900

    Add SQLAlchemy-backed protein DB and query helper

--- Database.py
+++ Database.py
@@ -1,35 +1,85 @@
-from Protein import Protein
+"""SQLAlchemy-backed protein database utilities."""
 
 from Bio import SeqIO
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.types import PickleType
+
+from Protein import Protein, describe
+
+Base = declarative_base()
+
+
+class ProteinModel(Base):
+    """SQLAlchemy model representing a protein."""
+
+    __tablename__ = "proteins"
+
+    id = Column(Integer, primary_key=True)
+    accession = Column(String, unique=True, index=True)
+    description = Column(String)
+    locus = Column(String)
+    organism = Column(String, index=True)
+    sequence = Column(String)
+    embedding = Column(PickleType)
+
 
 def read_fasta(filename):
-    data=[]
-    for record in SeqIO.parse(filename, 'fasta'):
-        data.append(Protein(record.id, record.description, record.locus, record.organism, record.seq))
+    """Read a FASTA file and return a list of Protein objects."""
+
+    data = []
+    for record in SeqIO.parse(filename, "fasta"):
+        accession, description, locus, organism = describe(record.description)
+        data.append(Protein(accession, description, locus, organism, str(record.seq)))
     return data
 
-class ProteinDB:
-    def __init__(self,fasta=None,protein=None):
-        if protein is None:
-            protein=[]
-        if fasta is None:
-            fasta=[]
-        else:
-            read_fasta(fasta)
-        self.proteins=fasta+protein
-        self.proteins.sort()
 
-    def __len__(self):
-        return len(self.proteins)
-    def __getitem__(self, index):
-        return self.proteins[index]
-    def __iter__(self):
-        return iter(self.proteins)
-    def __contains__(self, item):
-        return item in self.proteins
-    def __add__(self, other):
-        self.proteins.extend(other.proteins)
-        self.proteins=list(set(self.proteins))
-        self.proteins.sort()
-    def query(self,query):
+class ProteinDB:
+    """High level helper around a SQLAlchemy session."""
+
+    def __init__(self, url: str = "sqlite:///proteins.db"):
+        self.engine = create_engine(url)
+        Base.metadata.create_all(self.engine)
+        self.Session = sessionmaker(bind=self.engine)
+
+    def add_protein(self, protein: Protein) -> None:
+        """Insert a single Protein into the database."""
+
+        with self.Session() as session:
+            session.add(
+                ProteinModel(
+                    accession=protein.accession,
+                    description=protein.description,
+                    locus=protein.locus,
+                    organism=protein.organism,
+                    sequence=str(protein.sequence),
+                    embedding=protein.Z,
+                )
+            )
+            session.commit()
+
+    def add_from_fasta(self, fasta_path: str) -> None:
+        """Read proteins from a FASTA file and insert them into the DB."""
+
+        proteins = read_fasta(fasta_path)
+        with self.Session() as session:
+            session.add_all(
+                [
+                    ProteinModel(
+                        accession=p.accession,
+                        description=p.description,
+                        locus=p.locus,
+                        organism=p.organism,
+                        sequence=str(p.sequence),
+                        embedding=p.Z,
+                    )
+                    for p in proteins
+                ]
+            )
+            session.commit()
+
+    def session(self):
+        """Return a new session object."""
+
+        return self.Session()
 
